3f2bd8ccd784b8b4971af95b3f955bdd41d406f6
Arielcesde <adelarosa@cesde.net>
2026-09-09 20:38:50 -0500

    Se crea proyecto Obrimotos para la creacion de las reglas de negocios

---
+++ src/main/java/com/example/orbimotos/OrbimotosApplication.java
@@ -0,0 +1,13 @@
+package com.example.orbimotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbimotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbimotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orbimotos/OrbimotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orbimotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbimotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
